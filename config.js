2570a8fcf863da4bb81811e0e930737f7174931d
hechenglong <hechenglong@huice.com>
2023-02-13 14:16:31 +0800

    feat: init

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  cookie: '_ga=GA1.2.454240803.1644151773; _gid=GA1.2.745918689.1646218996; uid=28310; email=1030110207%40qq.com; key=d19034b5be4868f5d2ca6f47e2e26eb40586ba8e25a88; ip=2979db7d09326c5e9c549c56c3208eb7; expire_in=1646305398; _gat=1',
+  PUSH_PLUS_TOKEN: 'fa66a69b3ae444ca8f6b3b40978e7a32',
+  aid: '2608',
+  uuid: '7061857235442812453',
+  _signature: '_02B4Z6wo00901u5.W8QAAIDDlrGwwReX7j7ue19AANmaAXWGGkmn-jFq.w.tzD5E6dbVrnWOGxNhmKL3P4Ie31UNm8lX5qdwNLWwhEnm-x1Gkt936MQGoTKKGcWyeiiAln0cu8l23-Pr80.i95'
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+'use strict';
+exports.handler = async (event, context, callback) => {
+  require('./app.js')
+}
